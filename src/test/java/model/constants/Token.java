649770be1473cd158cdc56ac16f05540400210cd
AndreyD <andreyndyakonov@gmail.com>
2025-07-20 19:03:03 +0300

    add pom, Token and Lessons Test to repository

---
+++ src/test/java/model/constants/Token.java
@@ -0,0 +1,7 @@
+package model.constants;
+
+public class Token {
+
+    public static final String ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODU4M2Y4MmZkYTZjMjAwM2RjN2E4MDEiLCJpYXQiOjE3NTMwMjY4NzAsImV4cCI6MTc1MzYzMTY3MH0.eTk34-JQmwCpZTbSpY4-qULgxT1VnhE-v-LAm3Jmvhc";
+
+}
